086622d36997bb66886b67ef464c08ff48573658
EduardF1 <fischer_eduard@yahoo.com>
2021-09-04 14:12:03 +0200

    add validation to dto

--- src/components/cars/dto/new-car.input.ts
+++ src/components/cars/dto/new-car.input.ts
@@ -1,4 +1,5 @@
 import { Field, InputType, Int } from '@nestjs/graphql';
+import { Max, Min } from 'class-validator';
 
 @InputType()
 export class NewCarInput {
@@ -6,9 +7,13 @@ export class NewCarInput {
   name: string;
 
   @Field((type) => Int)
+  @Max(20000, { message: 'Daily price above minimum required amount.' })
+  @Min(1500, { message: 'Daily price below minimum required amount.' })
   dailyPrice: number;
 
   @Field((type) => Int)
+  @Max(1000, { message: 'Monthly price above minimum required amount.' })
+  @Min(10, { message: 'Monthly price below minimum required amount.' })
   monthlyPrice: number;
 
   @Field()
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { BaseExceptionFilter, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalFilters(new BaseExceptionFilter());
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(process.env.PORT || 9000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 
 bootstrap();
